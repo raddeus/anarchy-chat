566bcf6abaff7f2b1568d630fc3743a21536fa09
raddeus <thadblankenship@gmail.com>
2021-11-08 19:57:45 -0800

    Add cert

--- config/runtime.exs
+++ config/runtime.exs
@@ -13,5 +13,8 @@ config :websocket_playground, WebsocketPlayground.Repo,
   hostname: System.get_env("DB_HOST"),
   port: System.get_env("DB_PORT", "5432"),
   ssl: true,
+  ssl_opts: [
+    certfile: "priv/cert.crt"
+  ],
   log: false,
   pool_size: 10
