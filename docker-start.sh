be43919d0152f16be5e62362173eadb722e7affe
raddeus <thadblankenship@gmail.com>
2021-11-09 16:40:44 -0800

    Build npm at container runtime

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-_build/prod/rel/websocket_playground/bin/websocket_playground eval "WebsocketPlayground.Release.migrate"
-_build/prod/rel/websocket_playground/bin/websocket_playground start
+npm --prefix /app/client ci
+npm --prefix /app/client run build
+
+/app/_build/prod/rel/websocket_playground/bin/websocket_playground eval "WebsocketPlayground.Release.migrate"
+/app_build/prod/rel/websocket_playground/bin/websocket_playground start
