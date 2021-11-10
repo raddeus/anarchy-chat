f0ef26fe905dc1bd4b7279a415f810dabf52c6ab
raddeus <thadblankenship@gmail.com>
2021-11-09 16:56:06 -0800

    Ensure ssl is started before running migrations

--- docker-start.sh
+++ docker-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 /app/_build/prod/rel/websocket_playground/bin/websocket_playground eval "WebsocketPlayground.Release.migrate"
-/app_build/prod/rel/websocket_playground/bin/websocket_playground start
+/app/_build/prod/rel/websocket_playground/bin/websocket_playground start
